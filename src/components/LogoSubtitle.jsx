9f697ac57e11bf73ac10a902dff0fb77eface60b
bakwanj4gung <alimuhammadfaqih@gmail.com>
2025-07-21 20:16:45 +0700

    change font size logo subtitle

--- src/components/LogoSubtitle.jsx
+++ src/components/LogoSubtitle.jsx
@@ -4,7 +4,7 @@ function LogoSubtitle() {
     return (
         <div className="flex items-center gap-2 font-main-subtitle-logo font-bold text-center">
             <div className="h-0.5 border-t-2 border-orange-950 flex-1"></div>
-            <p className="whitespace-nowrap font-bold tracking-wider text-sm uppercase max-w-96 truncate">Make Your Own News blablablablablalbablalbalbalbalb</p>
+            <p className="whitespace-nowrap font-bold tracking-wider text-xs uppercase max-w-96 truncate">Make Your Own News blablablablablalbablalbalbalbalb</p>
             <div className="h-0.5 border-t-2 border-orange-950 flex-1"></div>
         </div>
     )
